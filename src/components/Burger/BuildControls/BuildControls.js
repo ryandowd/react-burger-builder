e21fa52ff3bf8375bb29844a7b7d7a91abe811b8
ryandowd <ryan.dowd84@gmail.com>
2018-10-04 17:49:10 +0800

    Adding 'Burger' component and 'BurgerControls' components. Also added 'Ingredients' components.

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Meat', type: 'meat' },
+];
+
+const buildControls = () => (
+  <div className={classes.BuildControls}>
+    {controls.map(ctrl => (
+      <BuildControl key={ctrl.type} label={ctrl.label}/>
+    ))}
+  </div>
+);
+
+export default buildControls;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 
