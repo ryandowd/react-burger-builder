3986afdad189a2bd619b0d0bca9d062e8ca675fa
ryandowd <ryan.dowd84@gmail.com>
2018-10-05 12:50:27 +0800

    Adding handlers to BurgerControls. Added pricing

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -10,10 +10,16 @@ const controls = [
   { label: 'Meat', type: 'meat' },
 ];
 
-const buildControls = () => (
+const buildControls = ( props ) => (
   <div className={classes.BuildControls}>
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
     {controls.map(ctrl => (
-      <BuildControl key={ctrl.type} label={ctrl.label}/>
+      <BuildControl 
+        key={ctrl.type} 
+        label={ctrl.label} 
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)} 
+        disabled={props.disabled[ctrl.type]} />
     ))}
   </div>
 );
